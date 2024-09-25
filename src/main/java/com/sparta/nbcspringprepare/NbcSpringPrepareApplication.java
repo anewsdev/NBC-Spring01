748292e8997f748936de628de359bf47600d1abf
anewsdev <anewsdev@gmail.com>
2024-09-25 15:08:49 +0900

    init commit

---
+++ src/main/java/com/sparta/nbcspringprepare/NbcSpringPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcspringprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcSpringPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcSpringPrepareApplication.class, args);
+    }
+
+}
